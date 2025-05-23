5acfeb5e4db8662ca18fe83287021150bb80be22
geun <1416geun@naver.com>
2025-05-23 10:55:31 +0900

    ,

--- html/admin/js/demo/datatables-demo.js
+++ html/admin/js/demo/datatables-demo.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   $('#dataTable').DataTable({
     language: {
       url: "//cdn.datatables.net/plug-ins/1.13.7/i18n/ko.json"
-    }
+    },
+    searching: false
   });
 });
