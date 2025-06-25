0b6e735aae2098d23c878ce846633743e5acd8e8
geun <1416geun@naver.com>
2025-06-25 14:56:56 +0900

    admin ui

--- html/admin/js/demo/datatables-demo.js
+++ html/admin/js/demo/datatables-demo.js
@@ -2,8 +2,12 @@
 $(document).ready(function () {
   $('#dataTable').DataTable({
     language: {
-      url: "//cdn.datatables.net/plug-ins/1.13.7/i18n/ko.json"
+      url: "https://cdn.datatables.net/plug-ins/1.13.7/i18n/ko.json"
     },
-    searching: false
+    searching: false,
+    dom: 'rt<"d-flex justify-content-between align-items-center table-footer"'
+       + '<"col-2"l>'
+       + '<"col-8 text-center"<"d-inline-block"p>>'
+       + '<"col-2 text-end"i>>'
   });
 });
